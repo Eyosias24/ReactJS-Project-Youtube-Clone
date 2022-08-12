4b8b1a81f8e7e472ddb3865576c23ae06fa0b4fa
Eyosias24 <eyosias24@gmail.com>
2022-08-12 16:56:28 +0300

    Added header navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-function App() {
-  return <div>Hello World</div>;
+import React, { Component } from "react";
+import HeaderNav from "./containers/HeaderNav/HeaderNav";
+
+class App extends Component {
+  render() {
+    return <HeaderNav />;
+  }
 }
 
 export default App;
+
+//  return <HeaderNav />;
